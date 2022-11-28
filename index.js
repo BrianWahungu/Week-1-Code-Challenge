b330f88ebcf673eabc7a0c4ec1980713661da438
BrianWahungu <cepteinhawks@gmail.com>
2022-11-28 19:23:51 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+// Challenge 1: Student Grade Generator (Toy Problem)39                          
+
+function marks(){
+let grade= prompt ("Enter marks");
+let result;
+if (grade>79 ){
+    result ="A"
+}
+if (grade <= 79 && grade >= 60){
+    result="B"
+}
+if (grade <= 59 && grade >=49){
+    result ="C"
+}
+if (grade <= 49 && grade >=40){
+    result="D"
+}
+if (grade<40){
+    result ="E"
+}
+document.getElementById("demo").innerHTML = result;
+}
+
+
+
+
+
